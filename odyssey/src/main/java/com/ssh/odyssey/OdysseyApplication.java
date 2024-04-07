5e1dd9c0ab41a3d9c9772050c71ce36f46f201a2
Getuar Kelmendi <getuarkelmendi6@gmail.com>
2024-04-08 01:10:34 +0200

    ODY-8:App initialiser

---
+++ odyssey/src/main/java/com/ssh/odyssey/OdysseyApplication.java
@@ -0,0 +1,13 @@
+package com.ssh.odyssey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdysseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdysseyApplication.class, args);
+	}
+
+}
