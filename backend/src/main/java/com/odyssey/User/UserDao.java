12f9b7d3960df202bf8c7bd78c693f7308a768ac
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2024-04-10 02:37:06 +0200

    ODY-5: Created and tested Data Access Objects, for users to store information in database.

--- backend/src/main/java/com/odyssey/User/UserDao.java
+++ backend/src/main/java/com/odyssey/User/UserDao.java
@@ -3,7 +3,14 @@ package com.odyssey.User;
 import com.odyssey.User.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserDao{
     List<User> selectAllUsers();
+    Optional<User> selectUserById(Integer Id);
+    void insertUser(User user);
+    boolean existsUserByEmail(String email);
+    boolean existsUserById(Integer Id);
+    boolean existsUserByUsername(String username);
+
 }
